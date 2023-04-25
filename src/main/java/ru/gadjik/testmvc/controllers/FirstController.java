16a4426478eea7bd9450bc7f437fa4a18d9fd312
Gadjikr <105589080+Gadjikr@users.noreply.github.com>
2023-04-26 01:28:27 +0300

    ver 0.3 requests. get with parameters to console

--- src/main/java/ru/gadjik/testmvc/controllers/FirstController.java
+++ src/main/java/ru/gadjik/testmvc/controllers/FirstController.java
@@ -3,6 +3,9 @@ package ru.gadjik.testmvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Gadjikr
@@ -12,12 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        System.out.println("Hello " + name + " " + surname + " !");
         return "/first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage(){
+    public String goodbyePage(
+            @RequestParam(value = "name", required = false)  String name,
+            @RequestParam(value = "surname",required = false) String surname){
+        // It will be error if GET parameters doesn't exist, "required = false" solve this problem
+        System.out.println("Bye  " + name + " " + surname + " !");
         return "/first/goodbye";
     }
 
